eee66809838b36bc00cea7a0fbc840ea6e4ea0b1
Sebastian Baier <sebastian.baier93@hotmail.de>
2020-04-08 17:15:19 +0200

    Adds Zenject. Adds Button Input detection

---
+++ Runtime/ButtonInput/BasicButtonInputDetector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace SBaier.Input
+{
+	public class BasicButtonInputDetector : ButtonInputDetector
+	{
+		private KeyCode _key;
+		private float _stateDuration = 0;
+
+		[Inject]
+		private void Construct(KeyCode key)
+		{
+			_key = key;
+		}
+
+		protected virtual void Update()
+		{
+			if (UnityEngine.Input.GetKeyDown(_key))
+			{
+				OnDown?.Invoke(createArg(ButtonState.Down));
+			}
+			else if(UnityEngine.Input.GetKey(_key))
+			{
+				_stateDuration += Time.deltaTime;
+				OnPress?.Invoke(createArg(ButtonState.Pressed));
+			}
+			else if(UnityEngine.Input.GetKeyUp(_key))
+			{
+				OnReleased?.Invoke(createArg(ButtonState.Released));
+				_stateDuration = 0;
+			}
+		}
+
+		private ButtonInputEventArgs createArg(ButtonState state)
+		{
+			return new ButtonInputEventArgs(_key, state, _stateDuration);
+		}
+
+
+		public override KeyCode Key => _key;
+
+		public override event Action<ButtonInputEventArgs> OnDown;
+		public override event Action<ButtonInputEventArgs> OnPress;
+		public override event Action<ButtonInputEventArgs> OnReleased;
+	}
+}
---
+++ Runtime/ButtonInput/ButtonInputRegistry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace SBaier.Input
+{
+	public abstract class ButtonInputRegistry : InputRegistry
+	{
+		public abstract void Subscribe(KeyCode code, ButtonState state, Action<ButtonInputEventArgs> listener);
+		public abstract void Unsubscribe(KeyCode code, ButtonState state, Action<ButtonInputEventArgs> listener);
+	}
+}
